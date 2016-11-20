ae314c7d4c69c1b17084202ce0a0b6ee5ae94345
Randy Wressell <rwressell@globesherpa.com>
2016-11-20 00:28:08 -0800

    Update diceRoller

--- js/generator.js
+++ js/generator.js
@@ -1,8 +1,17 @@
 'use strict';
+var diceExpression = document.getElementById('expresssionInput').value;
 
-function diceRoller(diceExpression) {
+function diceRoller() {
   //this should take expressions like 3d6 etc.
-
+  var diceExpression = document.getElementById('expresssionInput').value;
+  var count = diceExpression.match(/^[^d]*/)[0] || 1;
+  // console.log(count);
+  var die = diceExpression.match(/d(.[^+\-*\/\s]*)/)[1];
+  var total = 0;
+  for (var i = 0; i < count; i++) {
+    total += rollDice(die);
+  }
+  document.getElementById('diceValue').innerHTML = total;
 };
 
 function rollDice(sides) {
