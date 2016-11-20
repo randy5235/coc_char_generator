0cc2ded8ced50bf5264a0e61fec24ef3438be26a
Randy Wressell <rwressell@globesherpa.com>
2016-11-19 19:48:40 -0800

    First Commit

---
+++ js/generator.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function diceRoller(diceExpression) {
+  //this should take expressions like 3d6 etc.
+
+};
+
+function rollDice(sides) {
+  return Math.floor(Math.random() * (sides) + 1);
+};
